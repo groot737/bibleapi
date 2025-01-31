978a87817de169c7e3decd01bae0fdebb7d6885f
Giorgi <giorgiquchuloria7@gmail.com>
2025-01-31 04:28:33 +0400

    added bible demo reader

--- routes/pages.js
+++ routes/pages.js
@@ -7,4 +7,8 @@ router.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../views/index.html'))
 })
 
+router.get('/bible', (req,res) =>{
+    res.sendFile(path.resolve(__dirname, '../views/bible.html'))
+})
+
 module.exports = router;
