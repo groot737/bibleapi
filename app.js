bbd4dbfb792a7bbc209ba6b361cecdaa077defbc
Groot737 <giorgiquchuloria7@gmail.com>
2026-07-12 17:25:29 +0400

    Modified for vercel hosting

--- app.js
+++ app.js
@@ -36,6 +36,9 @@ app.use(cors());
 app.use('/api', apiRoute);
 app.use('/', page)
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
+}
+module.exports = app;
